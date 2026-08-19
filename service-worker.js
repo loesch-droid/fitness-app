8ffd89c23759bafbaf58cc2693e313da9dc3160c
loesch-droid <loesch78@gmail.com>
2026-08-19 13:08:15 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,41 @@
+const CACHE_NAME = "fitness-planner-cache-v1";
+
+const ASSETS = [
+  "index.html",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
+
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("fitness-cache").then(cache => {
-      return cache.addAll([
-        "/index.html",
-        "/manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(cacheRes => {
+      return cacheRes || fetch(event.request);
     })
   );
 });
